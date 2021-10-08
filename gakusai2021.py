76e42d716cbd307466b8915d1ea4253a1a1e9443
Takuma-86 <satotaT7335@gmail.com>
2021-10-08 21:56:33 +0900

    add gun_degree

--- gakusai2021.py
+++ gakusai2021.py
@@ -22,15 +22,17 @@ pi.set_PWM_range(B_ENABLE, 100)
 pi.set_mode(SERVO,pigpio.OUTPUT)
 pi.set_PWM_frequency(SERVO,50)
 
-pi.set_PWM_range(SERVO,2000) #-90°:50 0°:145 90°:240 5°刻み(19刻み)で動く
+pi.set_PWM_range(SERVO,2000)
 
-pulse = 145 #初期設定（砲塔0°、モーター停止）
+gun_degree = 0 #砲塔の角度 -90~90 5°刻み
+n = 0 #砲塔の動き -18~18 pulse=145+n*95/18
+pulse = 145 #初期設定（砲塔0°）
 pi.set_PWM_dutycycle(SERVO,145)
 
 right = 1 #仮置き、エラーでるの気持ち悪いから
 left = 1
 radius = 1
-angle = 0
+stick_degree = 0
 shot = 0
 
 def main():
@@ -45,7 +47,7 @@ def main():
             print(movementJsonCode["shoot"])
             print(movementJsonCode["LR"])
             #以下、入力に対して機体を動かすプログラム
-            #変数left,right,shot,radius,angle
+            #変数left,right,shot,radius,stick_degree
             if shot == 1:
                 shot()
 
@@ -69,17 +71,23 @@ def shot(): #レーザーガン班
     return
 
 def right_rotation(): #砲塔の右旋回
-    global pulse
-    if pulse < 240:
-        pulse += 19
+    global n
+    global gun_degree
+    if n < 18:
+        n += 1
+        pulse = round(145 + n*95/18)
+        gun_degree = n*5
         pi.set_PWM_dutycycle(SERVO,pulse)
         time.sleep(0.1)
         return
 
 def left_rotation(): #砲塔の左旋回
-    global pulse
-    if pulse > 50:
-        pulse -= 19
+    global n
+    global gun_degree
+    if n > -18:
+        n -= 1
+        pulse = round(145 + n*95/18)
+        gun_degree = n*5
         pi.set_PWM_dutycycle(SERVO,pulse)
         time.sleep(0.1)
         return
@@ -90,32 +98,32 @@ def stop():
         pi.write(B_ENABLE, 0)
         return
 
-def move(radius, angle):
-    if 0 <= angle < 15: #直進
+def move(radius, stick_degree):
+    if 0 <= stick_degree < 15: #直進
         rightPwm = 100
         leftPwm = 100
-    elif 15<= angle < 75: #右前方
-        rightPwm = 100 - (angle - 15)
+    elif 15<= stick_degree < 75: #右前方
+        rightPwm = 100 - (stick_degree - 15)
         leftPwm = 100
-    elif 75 <= angle < 105: #右旋回
+    elif 75 <= stick_degree < 105: #右旋回
         rightPwm = 50
         leftPwm = 50
-    elif 105 <= angle < 165: #右後方
-        rightPwm = 100 - (165 - angle)
+    elif 105 <= stick_degree < 165: #右後方
+        rightPwm = 100 - (165 - stick_degree)
         leftPwm = 100
-    elif 165 <= angle < 195: #後退
+    elif 165 <= stick_degree < 195: #後退
         rightPwm = 100
         leftPwm = 100
-    elif 195 <= angle < 255: #左後方
+    elif 195 <= stick_degree < 255: #左後方
         rightPwm = 100
-        leftPwm = 100 - (angle - 195)
-    elif 255 <= angle < 285: #左旋回
+        leftPwm = 100 - (stick_degree - 195)
+    elif 255 <= stick_degree < 285: #左旋回
         rightPwm = 50
         leftPwm = 50
-    elif 285 <= angle < 345: #左前方
+    elif 285 <= stick_degree < 345: #左前方
         rightPwm = 100
-        leftPwm = 100 - (345 - angle)
-    elif 345 <= angle < 360: #直進
+        leftPwm = 100 - (345 - stick_degree)
+    elif 345 <= stick_degree < 360: #直進
         rightPwm = 100
         leftPwm = 100
 
@@ -126,16 +134,16 @@ def move(radius, angle):
     pi.set_PWM_dutycycle(A_ENABLE, rightPwm)
     pi.set_PWM_dutycycle(B_ENABLE, leftPwm)
 
-    if 0 <= angle < 75 or 285 <= angle < 360: #前方
+    if 0 <= stick_degree < 75 or 285 <= stick_degree < 360: #前方
         pi.write(A_PHASE, 0)
         pi.write(B_PHASE, 1)
-    elif 105 <= angle < 255: #後方
+    elif 105 <= stick_degree < 255: #後方
         pi.write(A_PHASE, 1)
         pi.write(B_PHASE, 0)
-    elif 75 <= angle < 105: #右旋回
+    elif 75 <= stick_degree < 105: #右旋回
         pi.write(A_PHASE, 1)
         pi.write(B_PHASE, 1)
-    elif 255 <= angle < 285: #左旋回
+    elif 255 <= stick_degree < 285: #左旋回
         pi.write(A_PHASE, 0)
         pi.write(B_PHASE, 0)
     time.sleep(0.1)
